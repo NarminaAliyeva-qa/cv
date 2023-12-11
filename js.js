da99165e698ddb23fabb2e11f35aacbef7ffe9d2
NarminaAliyeva-qa <153552650+NarminaAliyeva-qa@users.noreply.github.com>
2023-12-11 23:26:49 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('One step at a time, with each step marked by a question and illuminated by an answer.')
+})
